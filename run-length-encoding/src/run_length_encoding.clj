a0a37159e809e04c0de831bf130aac2e4a3e38fc
Oleh Kuchuk <olehkuchuk@DevOpss-MacBook-Pro.local>
2018-07-24 19:38:26 +0300

    Run length encoding

---
+++ run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,10 @@
+(ns run-length-encoding)
+
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [plain-text]
+  (complement contains? plain-text "A"))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text])
---
+++ run-length-encoding/src/run_length_encoding.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+def run_length_encode(string):
+    """Encode raw string in format of 12A2B"""
+
+    counter = defaultdict(int)
+
+    for char in string:
+        counter[char] += 1
+
+    parts = [
+        str(count), key
+        for key, count in counter.items()
+    ]
+
+    return "".join(parts)
+        
+    
