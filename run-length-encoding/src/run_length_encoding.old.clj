b38c80a126dadf35cc0c9dd81f985e906358cf7b
Oleh Kuchuk <kuchuklehjs@gmail.com>
2018-07-25 19:01:34 +0300

    run length decoding

--- run-length-encoding/src/run_length_encoding.old.clj
+++ run-length-encoding/src/run_length_encoding.old.clj
@@ -4,16 +4,9 @@
   "encodes a string with run-length-encoding"
   [plain-text]
   (->> (partition-by identity plain-text)
-       (map (juxt count first))
-       (flatten)
+       (mapcat (juxt count first))
        (apply str)))
-
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [cipher-text]
-  (->> (re-seq #"(\d+)?([a-zA-Z])" cipher-text)
-       (map (partial drop 1))
-       (map (fn [[x y]] (repeat (Integer. (or x 1)) y)))
-       (flatten)
-       (apply str)))
+  [cipher-text])
